20b56dd09372a2474285599db520c2c5c17412ff
Habib ur Rehman <hbieb949@gmail.com>
2023-07-03 19:44:54 +0500

    Adding Exception handling for unexpected data

--- bot_identifier.py
+++ bot_identifier.py
@@ -291,6 +291,14 @@ class bot_identifier():
 
         # self.bot_df = pd.DataFrame(columns=["user_name", "repo_name", "pr_link", "bot_level"])
         for index, row in track(df.iterrows(), description="Bot identification...", total=df.shape[0]):
+            if type(row["comment_body"]) is float:
+                raise Exception('For Index {}, User {} in Repo: {}, PullNumber: {}, the comment body is:\n {}'
+                                .format(index, row["user"], row["repo_name"], row["pull_number"], row["comment_body"]))
+            
+            if type(row["user"]) is float:
+                raise Exception('For Index {}, Repo: {}, PullNumber: {}, the user is:\n {}'
+                                .format(index, row["repo_name"], row["pull_number"], row["user"]))
+            
             self.mathcer(row["user"], row["comment_body"].lower(), row["repo_name"], row["pull_number"], step=step)
 
         self.bot_df.to_csv(self.outputpath, index=False)
@@ -300,16 +308,21 @@ if __name__ == "__main__":
     # nltk.download("wordnet")
     
     # Input File
-    df = pd.read_csv("./data/results/collected_data_one_year_microsoft_May1st.csv") # here is the raw dataset file path
+    # df = pd.read_csv("./data/results/collected_data_one_year_microsoft_May1st.csv") # here is the raw dataset file path
+    df = pd.read_csv("/Users/rehmanh/Desktop/Research/Chenhao Bot Study/collected_data_one_year_microsoft_May1st-3.csv") # here is the raw dataset file path
     
     # Init the object
     bot_identification = bot_identifier()
-    
+
     # Set window size
     bot_identification.setWindowSize(10) # adjust it based on your evaluation results
 
     # Output File
-    bot_identification.setOuput("./data/results/bot_identification_MS200.csv") # modify the output path as you wish
+    # bot_identification.setOuput("./data/results/bot_identification_MS200.csv") # modify the output path as you wish
+    bot_identification.setOuput("/Users/rehmanh/Desktop/Research/Chenhao Bot Study/data/results/bot_identification_MS200_Habib.csv") # modify the output path as you wish
     
     # Run the Script
-    bot_identification.run(df, step=3) # if you want to put the developer account in the final result, then set the step to 4
+    try:
+        bot_identification.run(df, step=3) # if you want to put the developer account in the final result, then set the step to 4
+    except Exception as ex:
+        print(ex)
